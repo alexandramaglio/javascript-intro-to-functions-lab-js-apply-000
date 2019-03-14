3d0a4faa4e6b6ed0eaa47b1d351fad3ff7496094
Alexandra Middleton <alexandra.middleton@blackrock.com>
2019-03-14 01:29:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if(whisper(string)===string) {
-    return 'I can't hear you!''
+    return "I can't hear you!"
   }
   if(shout(string)===string) {
-    return 'YES INDEED!'
+    return "YES INDEED!"
   }
 }
